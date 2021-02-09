a06d066c78c2b9dc990207915044c633847119c2
BanurekhaV <rekhasairam@hotmail.com>
2021-02-09 18:10:13 +0530

    C# Day1

---
+++ C# Day1/FirstPrj/SecondPrj/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondPrj
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome to Project 2");
+            Console.Read();
+
+        }
+    }
+    class MySampleClass
+    {
+
+    }
+}
